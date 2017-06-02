529c9f678800d3e639fa7e58739c7afd7c1a538b
Debbie Milburn <debbiemilburn2@gmail.com>
2017-06-02 17:32:22 -0400

    Add association between appointment and doctor

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,3 +1,4 @@
 class Appointment < ActiveRecord::Base
   validates :patient, :doctor, :starts_at, :ends_at, presence: true
+  belongs_to :doctor
 end
--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -1,3 +1,4 @@
 class Doctor < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
+  has_many :appointments
 end
