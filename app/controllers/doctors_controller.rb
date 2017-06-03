000c0c87abd38413e347e3cc97b12ee63b211d1d
Debbie Milburn <debbiemilburn2@gmail.com>
2017-06-03 17:44:34 -0400

    Use time controller concern in doctor controller

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,13 +1,9 @@
 class DoctorsController < ApplicationController
+  include TimeControllerConcern
+
   def show
     doctor = Doctor.find_by(id: params[:id])
-    date = if params[:date]
-      params[:date]
-    else
-      Date.today
-    end
-    starts_at = date.to_datetime
-    ends_at = starts_at + 1
+    starts_at, ends_at = start_and_end_of_date(params[:date])
     render json: doctor.appointments.during(starts_at, ends_at)
   end
 end
