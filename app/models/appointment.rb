b22116333252a499d68f9e59296273632fcd6c94
Debbie Milburn <debbiemilburn2@gmail.com>
2017-06-03 17:16:37 -0400

    Create during scope for appointments

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -3,6 +3,7 @@ class Appointment < ActiveRecord::Base
   belongs_to :doctor
   validate :starts_at_after_ends_at, if: Proc.new { |appointment| appointment.ends_at && appointment.starts_at }
   validate :no_conflicting_appointments, if: Proc.new { |appointment| appointment.doctor }
+  scope :during, ->(starts_at, ends_at) { where("starts_at <= :ends_at and ends_at >= :starts_at",{:ends_at => ends_at, :starts_at => starts_at}) }
 
   def starts_at_after_ends_at
     errors.add(:ends_at, "can't be before starts at") if ends_at < starts_at
