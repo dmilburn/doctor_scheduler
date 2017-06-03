15bf2007015b01232816a673c8027cbc07dc31aa
Debbie Milburn <debbiemilburn2@gmail.com>
2017-06-02 22:05:10 -0400

    Create validation for overlapping appointments

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -8,4 +8,8 @@ class Appointment < ActiveRecord::Base
     errors.add(:ends_at, "can't be before starts at") if ends_at < starts_at
   end
 
+  def no_conflicting_appointments
+    errors.add(:base, "#{self.doctor.name} already has an appointment at that time") if Appointment.where("starts_at <= :ends_at and ends_at >= :starts_at and doctor_id = :doctor_id",{:ends_at => ends_at, :starts_at => starts_at,:doctor_id=>doctor_id})
+  end
+
 end
