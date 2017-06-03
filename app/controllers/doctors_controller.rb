64cae8409c1fb555ee7442fa65d01fe19990a075
Debbie Milburn <debbiemilburn2@gmail.com>
2017-06-03 17:40:07 -0400

    Add doctors show route to give back appointments for a doctor for a day

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,2 +1,13 @@
 class DoctorsController < ApplicationController
+  def show
+    doctor = Doctor.find_by(id: params[:id])
+    date = if params[:date]
+      params[:date]
+    else
+      Date.today
+    end
+    starts_at = date.to_datetime
+    ends_at = starts_at + 1
+    render json: doctor.appointments.during(starts_at, ends_at)
+  end
 end
