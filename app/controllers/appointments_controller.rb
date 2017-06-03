7371c836fb5613aedf1e2271a2c82d8e7b17cb82
Debbie Milburn <debbiemilburn2@gmail.com>
2017-06-03 17:46:47 -0400

    Add time controller concern to appointment controller

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,12 +1,8 @@
 class AppointmentsController < ApplicationController
+  include TimeControllerConcern
+
   def index
-    date = if params[:date]
-      params[:date]
-    else
-      Date.today
-    end
-    starts_at = date.to_datetime
-    ends_at = starts_at + 1
+    starts_at, ends_at = start_and_end_of_date(params[:date])
     render json: Appointment.during(starts_at, ends_at)
   end
 end
