31f6fb0285ec671df3ea40edb8c2c32fb1acaed7
Debbie Milburn <debbiemilburn2@gmail.com>
2017-06-04 14:11:27 -0400

    Get fake data displaying client side on calendar

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -1,3 +1,23 @@
 $(document).ready(function(){
-	$('#calendar').fullCalendar({});
+	$('#calendar').fullCalendar({
+		defaultView: 'agendaDay',
+		events:  [
+			        {
+			            title  : 'event1',
+			            start  : '2017-06-04'
+			        },
+			        {
+			            title  : 'event2',
+			            start  : '2017-06-04',
+			            end    : '2017-06-05'
+			        },
+			        {
+			            title  : 'event3',
+			            start  : '2017-06-04 12:30:00',
+			            end  : '2017-06-04 12:55:00',
+
+			            allDay : false
+			        }
+				],
+	});
 })
