56e18136bae58a1639f572367c7b69dc88ab8319
Debbie Milburn <debbiemilburn2@gmail.com>
2017-06-04 16:49:09 -0400

    Remove old doctor show controller method

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,7 +1,2 @@
 class DoctorsController < ApplicationController
-  def show
-    doctor = Doctor.find_by(id: params[:id])
-    starts_at, ends_at = start_and_end_of_date(params[:date])
-    render json: doctor.appointments.during(starts_at, ends_at)
-  end
 end
