b8afd690cc35c30caf2f228f385a8109c641a163
Debbie Milburn <debbiemilburn2@gmail.com>
2017-06-04 17:08:03 -0400

    Create calendar view for one doctor

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -17,4 +17,24 @@ $(document).ready(function(){
 		},
 		allDayDefault: false,
 	});
+
+	$('#calendar.one-doctor').fullCalendar({
+		defaultView: 'agendaDay',
+		events:  function(start, end, timezone, callback){
+			$.ajax({
+				url: $(location).attr('pathname') + '/appointments',
+				data: {
+					date: encodeURIComponent(start._d.toJSON()),
+				},
+				success: function(response){
+					var events = response.map(function(appointment){
+						return {title: appointment.patient, start: appointment.starts_at, end: appointment.ends_at}
+					})
+					callback(events);
+				}
+			})
+		},
+		allDayDefault: false,
+	});
+
 })
