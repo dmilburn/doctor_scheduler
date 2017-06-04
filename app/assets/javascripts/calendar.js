26856b60b1ea8ce9f1f67b21247d8fa751e755da
Debbie Milburn <debbiemilburn2@gmail.com>
2017-06-04 17:39:14 -0400

    Use CalendarOptions for single doctor event listener

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -25,23 +25,5 @@ $(document).ready(function(){
 
 	$('#calendar.all-doctors').fullCalendar(new CalendarOptions('/appointments'));
 
-	$('#calendar.one-doctor').fullCalendar({
-		defaultView: 'agendaDay',
-		events:  function(start, end, timezone, callback){
-			$.ajax({
-				url: $(location).attr('pathname') + '/appointments',
-				data: {
-					date: encodeURIComponent(start._d.toJSON()),
-				},
-				success: function(response){
-					var events = response.map(function(appointment){
-						return {title: appointment.patient, start: appointment.starts_at, end: appointment.ends_at}
-					})
-					callback(events);
-				}
-			})
-		},
-		allDayDefault: false,
-	});
-
+	$('#calendar.one-doctor').fullCalendar(new CalendarOptions($(location).attr('pathname') + '/appointments'));
 })
