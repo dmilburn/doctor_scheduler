07dc526cb7c511d78b25a1a42456c17217d7e151
Debbie Milburn <debbiemilburn2@gmail.com>
2017-06-04 17:36:18 -0400

    Create CalendarOptions to refactor all doctors calendar event listener

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -1,31 +1,29 @@
+function CalendarOptions(url) {
+	this.defaultView = 'agendaDay'
+	this.allDayDefault = false
+	this.events = this.eventsFetcher(url)
+}
+
+CalendarOptions.prototype.eventsFetcher = function(url) {
+	return function(start, end, timezone, callback){
+		$.ajax({
+			url: url,
+			data: {
+				date: encodeURIComponent(start._d.toJSON()),
+			},
+			success: function(response){
+				var events = response.map(function(appointment){
+					return {title: appointment.patient, start: appointment.starts_at, end: appointment.ends_at}
+				})
+				callback(events);
+			}
+		})
+	}
+};
+
 $(document).ready(function(){
 
-	var calendarOptions = {
-		defaultView: 'agendaDay',
-		allDayDefault: false,
-	}
-
-	var allDoctorsCalendarOptions = calendarOptions
-	allDoctorsCalendarOptions.events = eventsFetcher('/appointments');
-
-	function eventsFetcher(url) {
-		return function(start, end, timezone, callback){
-			$.ajax({
-				url: url,
-				data: {
-					date: encodeURIComponent(start._d.toJSON()),
-				},
-				success: function(response){
-					var events = response.map(function(appointment){
-						return {title: appointment.patient, start: appointment.starts_at, end: appointment.ends_at}
-					})
-					callback(events);
-				}
-			})
-		}
-	};
-
-	$('#calendar.all-doctors').fullCalendar(allDoctorsCalendarOptions);
+	$('#calendar.all-doctors').fullCalendar(new CalendarOptions('/appointments'));
 
 	$('#calendar.one-doctor').fullCalendar({
 		defaultView: 'agendaDay',
