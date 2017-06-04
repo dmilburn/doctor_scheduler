e4454fa32c4f2edbd7f8d03e0ef552b0cf12270f
Debbie Milburn <debbiemilburn2@gmail.com>
2017-06-04 14:28:06 -0400

    Retrieve real appointment data for today in calendar

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -1,26 +1,17 @@
 $(document).ready(function(){
 	$('#calendar').fullCalendar({
 		defaultView: 'agendaDay',
-		events:  [
-			        {
-			            title  : 'event1',
-			            start  : '2017-06-04',
-			            allDay: true,
-			        },
-			        {
-			            title  : 'event2',
-			            start  : '2017-06-04',
-			            end    : '2017-06-05',
-			            allDay : true,
-			        },
-			        {
-			            title  : 'event3',
-			            start  : '2017-06-04 12:30:00',
-			            end  : '2017-06-04 12:55:00',
-
-			        }
-				],
-
-			allDayDefault: false,
+		events:  function(start, end, timezone, callback){
+			$.ajax({
+				url: '/appointments',
+				success: function(response){
+					var events = response.map(function(appointment){
+						return {title: appointment.patient, start: appointment.starts_at, end: appointment.ends_at}
+					})
+					callback(events);
+				}
+			})
+		},
+		allDayDefault: false,
 	});
 })
