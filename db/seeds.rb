20ae2ac6dcb01f5fe40706716907cced9c680d9f
Debbie Milburn <debbiemilburn2@gmail.com>
2017-06-10 17:14:09 -0400

    update seed times

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,14 +2,14 @@
   doctor = Doctor.create(name: "Dr. #{Faker::LordOfTheRings.character}")
 end
 
-Doctor.first.appointments.create(starts_at: "2017-06-02 23:59:43.000000", ends_at: "2017-06-03 00:01:43.000000", patient: Faker::HarryPotter.character)
+Doctor.first.appointments.create(starts_at: "2017-06-11 23:59:43.000000", ends_at: "2017-06-12 00:01:43.000000", patient: Faker::HarryPotter.character)
 
-Doctor.second.appointments.create(starts_at: "2017-06-03 00:00:43.000000", ends_at: "2017-06-03 00:29:43.000000", patient: Faker::HarryPotter.character)
+Doctor.second.appointments.create(starts_at: "2017-06-12 00:00:43.000000", ends_at: "2017-06-12 00:29:43.000000", patient: Faker::HarryPotter.character)
 
-Doctor.second.appointments.create(starts_at: "2017-06-03 00:30:43.000000", ends_at: "2017-06-03 01:00:43.000000", patient: Faker::HarryPotter.character)
+Doctor.second.appointments.create(starts_at: "2017-06-12 00:30:43.000000", ends_at: "2017-06-12 01:00:43.000000", patient: Faker::HarryPotter.character)
 
-Doctor.second.appointments.create(starts_at: "2017-06-03 23:30:43.000000", ends_at: "2017-06-03 23:59:43.000000", patient: Faker::HarryPotter.character)
+Doctor.second.appointments.create(starts_at: "2017-06-12 23:30:43.000000", ends_at: "2017-06-12 23:59:43.000000", patient: Faker::HarryPotter.character)
 
-Doctor.third.appointments.create(starts_at: "2017-06-03 23:30:43.000000", ends_at: "2017-06-04 00:01:43.000000", patient: Faker::HarryPotter.character)
+Doctor.third.appointments.create(starts_at: "2017-06-12 23:30:43.000000", ends_at: "2017-06-13 00:01:43.000000", patient: Faker::HarryPotter.character)
 
-Doctor.fourth.appointments.create(starts_at: "2017-06-02 23:59:43.000000", ends_at: "2017-06-04 00:01:43.000000", patient: Faker::HarryPotter.character)
+Doctor.fourth.appointments.create(starts_at: "2017-06-11 23:00:43.000000", ends_at: "2017-06-13 00:01:43.000000", patient: Faker::HarryPotter.character)
