e734222e693efb95422eaae891090e2d15164347
Debbie Milburn <debbiemilburn2@gmail.com>
2017-06-10 14:25:19 -0400

    Add doctors to navbar

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -1,6 +1,13 @@
 class DoctorsController < ApplicationController
+  before_action :load_doctors
+
   def show
     @appointment = Appointment.new
     @doctor = Doctor.find_by(id: params[:id])
+  end
+
+  private
+  def load_doctors
+    @doctors = Doctor.all
   end
 end
