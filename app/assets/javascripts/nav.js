ca4a44c1137727f3bd2e0510b71a22cebca28334
Debbie Milburn <debbiemilburn2@gmail.com>
2017-06-11 09:58:01 -0400

    Remove test code

--- app/assets/javascripts/nav.js
+++ app/assets/javascripts/nav.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
 	$(".hamburger").click(function(){
-		console.log("hello")
 		$("#navbar").slideToggle("slow", function(){
 			$(".cross").show();
 			$(".hamburger").hide();
